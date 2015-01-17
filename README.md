6b1bbb468cedec33f94184757c84a062e3b682de
Sean Lip <sean@seanlip.org>
2015-01-17 13:04:02 -0500

    Allow processing of a particular cell from cmdline (see README)

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Code for The 10,000 Puzzle Pyramid
 
 Extract pyramid.zip inside this directory.
+
+To run a single test:
+
+    python runpuzzle.py -r {{ROW_NUMBER}} -c {{COL_NUMBER}}
+
+To solve the whole pyramid:
+
+    python runpuzzle.py
--- runpuzzle.py
+++ runpuzzle.py
@@ -1,8 +1,19 @@
+import argparse
 from os import listdir
 
 print 'Warming up...'
 
 from parsers import process_file
+
+_PARSER = argparse.ArgumentParser()
+_PARSER.add_argument(
+    '-r',
+    help='Row number of cell',
+    type=str)
+_PARSER.add_argument(
+    '-c',
+    help='Column number of cell',
+    type=str)
 
 # Set up pyramid.
 
@@ -19,9 +30,19 @@ print '---------------'
 print 'Solving pyramid'
 print '---------------'
 
-for dirname in listdir('pyramid'):
-    if dirname.startswith('row'):
-        row = int(dirname[3:])
-        for filename in listdir('pyramid/%s' % dirname):
-            col = int(filename[(len(dirname) + 4):][:-4])
-            pyramid[row][col] = process_file('pyramid/%s/%s' % (dirname, filename))
+def process_all():
+	for dirname in listdir('pyramid'):
+	    if dirname.startswith('row'):
+	        row = int(dirname[3:])
+	        for filename in listdir('pyramid/%s' % dirname):
+	            col = int(filename[(len(dirname) + 4):][:-4])
+	            pyramid[row][col] = process_file('pyramid/%s/%s' % (dirname, filename))
+
+
+parsed_args = _PARSER.parse_args()
+if not parsed_args.r or not parsed_args.c:
+	process_all()
+else:
+	process_file(
+		'pyramid/row%s/row%s_col%s.txt' %
+		(parsed_args.r, parsed_args.r, parsed_args.c))
