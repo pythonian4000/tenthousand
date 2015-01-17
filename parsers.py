16bb0d408c97aa95141fd88206dd607be56a3e24
Xinyu Wu <wxyxinyu@gmail.com>
2015-01-17 20:52:34 +0800

    anagrams

--- parsers.py
+++ parsers.py
@@ -12,12 +12,12 @@ m = hashlib.sha1()
 anagrams_0 = set()
 anagrams_1 = set()
 anagrams_2 = set()
-"""with open('anagrams-0.txt') as f:
+with open('anagrams-0.txt') as f:
     anagrams_0 = set(word.upper().rstrip() for word in f.readlines())
 with open('anagrams-1.txt') as f:
     anagrams_1 = set(word.upper().rstrip() for word in f.readlines())
 with open('anagrams-2.txt') as f:
-    anagrams_2 = set(word.upper().rstrip() for word in f.readlines())"""
+    anagrams_2 = set(word.upper().rstrip() for word in f.readlines())
 
 with open('country-codes.txt') as f:
     match_country_codes = set(word.upper().rstrip() for word in f.readlines())
@@ -36,6 +36,9 @@ scrabble_points = {
     'y':  4, 'z': 10,
 }
 
+anagrams_0 = set()
+anagrams_1 = set()
+anagrams_2 = set()
 caesar_shiftable = set()
 most_common_letter_counts = {}
 most_common_vowel_counts = {}
--- preprocess.py
+++ preprocess.py
@@ -1,44 +1,29 @@
-def anagrams(word):
-    if len(word) < 2:
-        yield word
-    else:
-        for i, letter in enumerate(word):
-            if not letter in word[:i]:
-                for j in anagrams(word[:i]+word[i+1:]):
-                    yield j + letter
-
 with open('pyramid/words.txt') as f:
     wordlist = set(word.upper().rstrip() for word in f.readlines())
 
-print 'Anagrams on their own'
 anagrams_0 = set()
+anagrams_1 = set()
+anagrams_2 = set()
+sorted_words_count = {}
 for word in wordlist:
-    for a in anagrams(word):
-        if a in wordlist:
-            anagrams_0.add(word + '/n')
-            break
+    sorted_word = ''.join(sorted(word))
+    if sorted_words_count.has_key(sorted_word):
+        sorted_words_count[sorted_word] += 1
+    else:
+        sorted_words_count[sorted_word] = 1
+for word in wordlist:
+    if sorted_words_count[''.join(sorted(word))] > 1:
+        anagrams_0.add(word + '\n')
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for wildcard in letters:
+        if sorted_words_count.has_key(''.join(sorted(word + wildcard))):
+            anagrams_1.add(word + '\n')
+        for wildcard2 in letters:
+            if sorted_words_count.has_key(''.join(sorted(word + wildcard + wildcard2))):
+                anagrams_2.add(word + '\n')
 with open('anagrams-0.txt', 'w') as f:
     f.writelines(anagrams_0)
-
-print 'Anagrams with one extra char'
-anagrams_1 = set()
-for word in wordlist:
-    for i in range(ord('a'), ord('z')+1):
-        for a in anagrams(word + chr(i)):
-            if a in wordlist:
-                anagrams_1.add(word + '/n')
-                break
 with open('anagrams-1.txt', 'w') as f:
     f.writelines(anagrams_1)
-
-print 'Anagrams with two extra chars'
-anagrams_2 = set()
-for word in wordlist:
-    for i in range(ord('a'), ord('z')+1):
-        for j in range(ord('a'), ord('z')+1):
-            for a in anagrams(word + chr(i) + chr(j)):
-                if a in wordlist:
-                    anagrams_2.add(word + '/n')
-                    break
 with open('anagrams-2.txt', 'w') as f:
     f.writelines(anagrams_2)
